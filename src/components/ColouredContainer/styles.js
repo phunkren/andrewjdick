e5809a08bcbcb47555208ffcb399f4b6a910469e
Andrew James Dick <andrewdick1987@gmail.com>
2019-01-26 11:23:57 +0000

    🐛  moved bgColor to attrs method

--- src/components/ColouredContainer/styles.js
+++ src/components/ColouredContainer/styles.js
@@ -1,7 +1,20 @@
 import styled from "styled-components";
 import { rgba } from "polished";
 
-export const Container = styled.main`
+export const Container = styled.main.attrs(
+  ({ clientX, clientY, clientWidth, clientHeight }) => ({
+    style: {
+      backgroundColor: rgba(
+        Math.round((clientX / clientWidth) * 255),
+        Math.round(
+          ((clientX + clientY / 2) / (clientHeight + clientWidth / 2)) * 255
+        ),
+        Math.round((clientY / clientHeight) * 255),
+        0.1
+      )
+    }
+  })
+)`
   display: flex;
   flex-direction: column;
   position: relative;
@@ -9,12 +22,4 @@ export const Container = styled.main`
   margin: 0;
   padding: 0;
   transition: background-color 0.2s ease-out;
-  background-color: ${({ clientX, clientY, clientWidth, clientHeight }) => {
-    const r = Math.round((clientX / clientWidth) * 255);
-    const b = Math.round((clientY / clientHeight) * 255);
-    const g = Math.round(
-      ((clientX + clientY / 2) / (clientHeight + clientWidth / 2)) * 255
-    );
-    return rgba(r, g, b, 0.1);
-  }};
 `;
