3f848855db17fad1635cf7ec4558bc36fdb6c681
Andrew James Dick <andrewdick1987@gmail.com>
2019-01-27 19:40:59 +0000

    🎨 fixed background color on mobile

--- src/components/ColouredContainer/styles.js
+++ src/components/ColouredContainer/styles.js
@@ -1,19 +1,31 @@
 import styled from "styled-components";
 import { rgba } from "polished";
+import { isBrowser } from "react-device-detect";
+import { COLORS } from "constants.js";
 
 export const Container = styled.main.attrs(
-  ({ clientX, clientY, clientWidth, clientHeight }) => ({
-    style: {
-      backgroundColor: rgba(
-        Math.round((clientX / clientWidth) * 255),
-        Math.round(
-          ((clientX + clientY / 2) / (clientHeight + clientWidth / 2)) * 255
-        ),
-        Math.round((clientY / clientHeight) * 255),
-        0.1
-      )
-    }
-  })
+  ({ clientX, clientY, clientWidth, clientHeight, style }) =>
+    isBrowser
+      ? {
+          style: {
+            ...style,
+            backgroundColor: rgba(
+              Math.round((clientX / clientWidth) * 255),
+              Math.round(
+                ((clientX + clientY / 2) / (clientHeight + clientWidth / 2)) *
+                  255
+              ),
+              Math.round((clientY / clientHeight) * 255),
+              0.1
+            )
+          }
+        }
+      : {
+          style: {
+            ...style,
+            backgroundColor: rgba(COLORS.white, 0.1)
+          }
+        }
 )`
   display: flex;
   flex-direction: column;
@@ -21,5 +33,6 @@ export const Container = styled.main.attrs(
   height: 100%;
   margin: 0;
   padding: 0;
+
   transition: background-color 0.2s ease-out;
 `;
