168bc4726630ea9a4509b8d2ebc163772eeb5b1a
Andrew James Dick <andrewdick1987@gmail.com>
2019-01-27 15:47:40 +0000

    🔧  register service worker

--- src/index.js
+++ src/index.js
@@ -64,4 +64,4 @@ ReactDOM.render(<App />, document.getElementById("wallop"));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
