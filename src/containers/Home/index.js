5a3d2bfd06ac4077f679fbef3bf00d0f55fc9403
Andrew James Dick <andrewdick1987@gmail.com>
2019-03-08 03:56:01 +0000

    ✨ Added animations to homepage

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,17 +1,31 @@
 import React, { Fragment } from "react";
+import { Spring } from "react-spring";
 import { ExternalLink } from "components/Link";
 import { TitleAndMetaTags } from "components/TitleAndMetaTags";
 import { EXPERIENCE } from "containers/CV/data";
 import { Social } from "./Social";
 import { Navigation } from "./Navigation";
 import { CONTACT_DETAILS } from "constants.js";
+import css3Icon from "./icons/css3.svg";
+import gatsbyIcon from "./icons/gatsby.svg";
+import gitIcon from "./icons/git.svg";
+import graphqlIcon from "./icons/graphql.svg";
+import html5Icon from "./icons/html5.svg";
+import javascriptIcon from "./icons/javascript.svg";
+import netlifyIcon from "./icons/netlify.svg";
+import npmIcon from "./icons/npm.svg";
+import reactIcon from "./icons/react.svg";
+import yarnIcon from "./icons/yarn.svg";
 import {
   Header,
   Section,
   Name,
   Info,
   Footer,
-  FooterNavigation
+  FooterNavigation,
+  Wrapper,
+  Image,
+  StyledParticles
 } from "./styles";
 
 export const Home = () => {
@@ -21,30 +35,134 @@ export const Home = () => {
   return (
     <Fragment>
       <TitleAndMetaTags />
+      <Wrapper>
+        <StyledParticles
+          params={{
+            retina_detect: true,
+            particles: {
+              shape: {
+                type: "images",
+                images: [
+                  {
+                    src: css3Icon,
+                    height: 10,
+                    width: 10
+                  },
+                  {
+                    src: gatsbyIcon,
+                    height: 10,
+                    width: 10
+                  },
+                  {
+                    src: gitIcon,
+                    height: 10,
+                    width: 10
+                  },
+                  {
+                    src: graphqlIcon,
+                    height: 10,
+                    width: 10
+                  },
+                  {
+                    src: html5Icon,
+                    height: 10,
+                    width: 10
+                  },
+                  {
+                    src: javascriptIcon,
+                    height: 10,
+                    width: 10
+                  },
+                  {
+                    src: netlifyIcon,
+                    height: 10,
+                    width: 10
+                  },
+                  {
+                    src: npmIcon,
+                    height: 10,
+                    width: 10
+                  },
+                  {
+                    src: reactIcon,
+                    height: 10,
+                    width: 10
+                  },
+                  {
+                    src: yarnIcon,
+                    height: 10,
+                    width: 10
+                  }
+                ]
+              },
+              move: {
+                direction: "top",
+                random: false,
+                straight: true,
+                out_mode: "out",
+                speed: 1,
+                attract: {
+                  enable: false
+                }
+              },
+              number: {
+                value: 10,
+                density: {
+                  enable: true,
+                  value_area: 1750
+                }
+              },
+              size: {
+                value: 40
+              }
+            }
+          }}
+        />
 
-      <Header>
-        <Navigation />
-        <Social />
-      </Header>
+        <Spring from={{ opacity: 0 }} to={{ opacity: 0.05 }}>
+          {props => <Image style={props} />}
+        </Spring>
 
-      <Section>
-        <Name>{name}</Name>
-        <Info>
-          {position} @{" "}
-          <ExternalLink
-            href={currentEmployer.url}
-            alt={`${currentEmployer.company}'s website`}
-            aria-label={`${currentEmployer.company}'s website`}
-          >
-            Fathom
-          </ExternalLink>
-        </Info>
-        <Info>{location}</Info>
-      </Section>
+        <Spring delay={2200} from={{ top: -75 }} to={{ top: 0 }}>
+          {props => (
+            <Header style={props}>
+              <Navigation />
+              <Social />
+            </Header>
+          )}
+        </Spring>
 
-      <Footer>
-        <FooterNavigation />
-      </Footer>
+        <Spring
+          delay={2000}
+          from={{ transform: "scale(0.9)", opacity: 0 }}
+          to={{ transform: "scale(1)", opacity: 1 }}
+        >
+          {props => (
+            <Section style={props}>
+              <Name>{name}</Name>
+              <Info>
+                {position} @{" "}
+                <ExternalLink
+                  href={currentEmployer.url}
+                  alt={`${currentEmployer.company}'s website`}
+                  aria-label={`${currentEmployer.company}'s website`}
+                >
+                  {currentEmployer.company}
+                </ExternalLink>
+              </Info>
+              <Info>{location}</Info>
+            </Section>
+          )}
+        </Spring>
+
+        <Spring delay={2700} from={{ bottom: -75 }} to={{ bottom: 0 }}>
+          {props => (
+            <Footer style={props}>
+              <FooterNavigation />
+            </Footer>
+          )}
+        </Spring>
+      </Wrapper>
     </Fragment>
   );
 };
--- src/containers/Home/styles.js
+++ src/containers/Home/styles.js
@@ -1,6 +1,58 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 import { Navigation } from "./Navigation";
+import homeBackground from "assets/images/homeBackground.png";
+import Particles from "react-particles-js";
 import { media } from "media.js";
+
+const infiniteScroll = keyframes`
+  from {
+    transform: translate3d(0, 0, 0);
+  }
+  to {
+    transform: translate3d(0, -1920px, 0);
+  }
+`;
+
+const particleOpacity = keyframes`
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 0.1;
+  }
+`;
+
+export const StyledParticles = styled(Particles)`
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  animation: ${particleOpacity} 2s linear;
+  pointer-events: none;
+  opacity: 0.1;
+`;
+
+export const Wrapper = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  position: relative;
+  overflow: hidden;
+`;
+
+export const Image = styled.div`
+  position: absolute;
+  right: 0;
+  left: 0;
+  top: 0;
+  height: 400%;
+  background-image: url(${homeBackground});
+  background-position: center center;
+  background-repeat: repeat
+  pointer-events: none;
+  animation: ${infiniteScroll} 120s linear infinite;
+`;
 
 export const Section = styled.section`
   flex: 1;
@@ -9,6 +61,7 @@ export const Section = styled.section`
   align-items: center;
   justify-content: center;
   margin: 10px;
+  transition: all 400ms linear;
 `;
 
 export const Header = styled.header`
@@ -16,7 +69,8 @@ export const Header = styled.header`
   justify-content: center;
   align-items: center;
   padding: 2em 2em 0;
-
+  position: relative;
+  transition: top 600ms linear;
   ${media.tablet`
     justify-content: space-between;
   `};
@@ -26,6 +80,8 @@ export const Footer = styled.footer`
   display: flex;
   justify-content: center;
   padding: 0 2em 2em;
+  position: relative;
+  transition: bottom 600ms linear;
 
   ${media.tablet`
     display: none;
@@ -60,7 +116,7 @@ export const Info = styled.p`
   text-align: center;
 
   ${media.tablet`
-    font-size: 2rem;
+    font-size: 1.8rem;
     line-height: 2.5rem;
   `};
 `;
