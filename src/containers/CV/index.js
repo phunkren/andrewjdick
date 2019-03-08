c4f5f6d46361d41efe7a25894eaacd2c9c918fbb
Andrew James Dick <andrewdick1987@gmail.com>
2019-03-08 14:49:05 +0000

    🎨 spring tweaks

--- src/containers/CV/index.js
+++ src/containers/CV/index.js
@@ -35,133 +35,138 @@ export const CV = () => {
     <Fragment>
       <TitleAndMetaTags title="CV" pathname="cv" />
       <ColouredContainer>
-        <Spring delay={500} from={{ opacity: 0 }} to={{ opacity: 1 }}>
-          {props => (
-            <Container style={props}>
-              <Header>
-                <HeaderTitle>
-                  <Link
-                    href="/"
-                    alt="Return to homepage"
-                    aria-label="Return to homepage"
-                  >
-                    <H1>{name}</H1>
-                  </Link>
-                  <Subtitle>
-                    {position} | {location}
-                  </Subtitle>
-                </HeaderTitle>
-
-                {isBrowser && !isIE && (
-                  <HeaderIcons>
+        <Container>
+          <Spring delay={500} from={{ opacity: 0 }} to={{ opacity: 1 }}>
+            {props => (
+              <Fragment>
+                <Header style={props}>
+                  <HeaderTitle>
                     <Link
-                      href="/AndrewJamesDick-CV.pdf"
-                      alt="Download my CV"
-                      aria-label="Download my CV"
-                      download
+                      href="/"
+                      alt="Return to homepage"
+                      aria-label="Return to homepage"
                     >
-                      <DownloadIcon width="2.25em" height="2.25em" />
+                      <H1>{name}</H1>
                     </Link>
-                  </HeaderIcons>
-                )}
-              </Header>
-              <Content>
-                <Sidebar>
-                  <Block>
-                    <MobileH2>Contact</MobileH2>
-                    <Contact />
-                  </Block>
+                    <Subtitle>
+                      {position} | {location}
+                    </Subtitle>
+                  </HeaderTitle>
 
-                  <Block>
-                    <H2>Education</H2>
-                    {EDUCATION.map(
-                      ({ qualification, course, institute, dates }, index) => (
-                        <Block key={`Education-${index}`}>
-                          <H3>{qualification}</H3>
-                          <Subtitle>{course}</Subtitle>
-                          <Subtitle>{institute}</Subtitle>
-                          <Text>{dates}</Text>
-                        </Block>
-                      )
-                    )}
-                  </Block>
-
-                  <Block>
-                    <H2>Expertise</H2>
+                  {isBrowser && !isIE && (
+                    <HeaderIcons>
+                      <Link
+                        href="/AndrewJamesDick-CV.pdf"
+                        alt="Download my CV"
+                        aria-label="Download my CV"
+                        download
+                      >
+                        <DownloadIcon width="2.25em" height="2.25em" />
+                      </Link>
+                    </HeaderIcons>
+                  )}
+                </Header>
+                <Content style={props}>
+                  <Sidebar>
                     <Block>
-                      <Rating skills={EXPERTISE} numberOfStars={5} />
+                      <MobileH2>Contact</MobileH2>
+                      <Contact />
                     </Block>
-                  </Block>
 
-                  <Block>
-                    <H2>Interests</H2>
                     <Block>
-                      {INTERESTS.map((interest, index) => (
-                        <Tag key={`Interest-${index}`}>{interest}</Tag>
-                      ))}
+                      <H2>Education</H2>
+                      {EDUCATION.map(
+                        (
+                          { qualification, course, institute, dates },
+                          index
+                        ) => (
+                          <Block key={`Education-${index}`}>
+                            <H3>{qualification}</H3>
+                            <Subtitle>{course}</Subtitle>
+                            <Subtitle>{institute}</Subtitle>
+                            <Text>{dates}</Text>
+                          </Block>
+                        )
+                      )}
                     </Block>
-                  </Block>
 
-                  <Block>
-                    <H2>Hobbies</H2>
                     <Block>
-                      {HOBBIES.map((interest, index) => (
-                        <Tag key={`Hobby-${index}`}>{interest}</Tag>
-                      ))}
+                      <H2>Expertise</H2>
+                      <Block>
+                        <Rating skills={EXPERTISE} numberOfStars={5} />
+                      </Block>
                     </Block>
-                  </Block>
 
-                  <Block>
-                    <H2>References</H2>
                     <Block>
-                      <Text>Written references available upon request.</Text>
+                      <H2>Interests</H2>
+                      <Block>
+                        {INTERESTS.map((interest, index) => (
+                          <Tag key={`Interest-${index}`}>{interest}</Tag>
+                        ))}
+                      </Block>
                     </Block>
-                  </Block>
-                </Sidebar>
 
-                <Section>
-                  <Block>
-                    <H2>Professional Profile</H2>
-                    <Text>
-                      My passion for digital technology continually drives me to
-                      advance my skill set as a software engineer. With strong
-                      communication and frontend web development skills, I
-                      thrive in environments where I can learn from and inspire
-                      those around me.
-                    </Text>
-                  </Block>
+                    <Block>
+                      <H2>Hobbies</H2>
+                      <Block>
+                        {HOBBIES.map((interest, index) => (
+                          <Tag key={`Hobby-${index}`}>{interest}</Tag>
+                        ))}
+                      </Block>
+                    </Block>
 
-                  <Block>
-                    <H2>Work Experience</H2>
-                    {EXPERIENCE.map(
-                      (
-                        { position, company, url, dates, description },
-                        index
-                      ) => (
-                        <Block key={`Experience-${index}`}>
-                          <H3>{position}</H3>
-                          <Subtitle>
-                            <ExternalLink
-                              href={url}
-                              alt={`${company} website`}
-                              aria-label={`${company} website`}
-                            >
-                              {company}
-                            </ExternalLink>{" "}
-                            / {dates}
-                          </Subtitle>
-                          {description && (
-                            <Description>{description()}</Description>
-                          )}
-                        </Block>
-                      )
-                    )}
-                  </Block>
-                </Section>
-              </Content>
-            </Container>
-          )}
-        </Spring>
+                    <Block>
+                      <H2>References</H2>
+                      <Block>
+                        <Text>Written references available upon request.</Text>
+                      </Block>
+                    </Block>
+                  </Sidebar>
+
+                  <Section>
+                    <Block>
+                      <H2>Professional Profile</H2>
+                      <Text>
+                        My passion for digital technology continually drives me
+                        to advance my skill set as a software engineer. With
+                        strong communication and frontend web development
+                        skills, I thrive in environments where I can learn from
+                        and inspire those around me.
+                      </Text>
+                    </Block>
+
+                    <Block>
+                      <H2>Work Experience</H2>
+                      {EXPERIENCE.map(
+                        (
+                          { position, company, url, dates, description },
+                          index
+                        ) => (
+                          <Block key={`Experience-${index}`}>
+                            <H3>{position}</H3>
+                            <Subtitle>
+                              <ExternalLink
+                                href={url}
+                                alt={`${company} website`}
+                                aria-label={`${company} website`}
+                              >
+                                {company}
+                              </ExternalLink>{" "}
+                              / {dates}
+                            </Subtitle>
+                            {description && (
+                              <Description>{description()}</Description>
+                            )}
+                          </Block>
+                        )
+                      )}
+                    </Block>
+                  </Section>
+                </Content>
+              </Fragment>
+            )}
+          </Spring>
+        </Container>
       </ColouredContainer>
     </Fragment>
   );
