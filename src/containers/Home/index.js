3b03cf955ac54bae6e75d225f0b9dd72c174deb4
Andrew James <contact@ajames.dev>
2020-01-01 23:11:13 +0000

    ✨ Added copyright to homepage

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -14,6 +14,7 @@ import {
   Footer,
   FooterSocial,
   BackgroundCredit,
+  Legal,
   Wrapper,
   Image,
   At
@@ -85,6 +86,7 @@ export class Home extends React.Component {
                 absurd.design
               </ExternalLink>
             </BackgroundCredit>
+            <Legal>&copy; 2020</Legal>
           </Footer>
         </Wrapper>
       </Fragment>
