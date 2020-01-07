4f64f6b03bd19fb4ab7b5a0ba5a4938374f155cd
Andrew James <contact@ajames.dev>
2020-01-07 08:25:08 +0000

    ✨ Added Google font fetch

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,20 @@
 module.exports = {
-  plugins: [`gatsby-plugin-styled-components`]
+  plugins: [
+    `gatsby-plugin-styled-components`,
+    {
+      resolve: `gatsby-plugin-prefetch-google-fonts`,
+      options: {
+        fonts: [
+          {
+            family: `Roboto Slab`,
+            variants: [`400`]
+          },
+          {
+            family: `Lato`,
+            variants: [`400`]
+          }
+        ]
+      }
+    }
+  ]
 };
