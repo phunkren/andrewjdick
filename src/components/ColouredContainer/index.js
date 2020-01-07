32e47cfe7e0a5d82b877c9f04ed1517c449939de
Andrew James <contact@ajames.dev>
2020-01-07 09:21:11 +0000

    🎨 Refactored coloured container to use hooks

--- src/components/ColouredContainer/index.js
+++ src/components/ColouredContainer/index.js
@@ -1,53 +1,46 @@
-import React, { Component } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { Container } from "./styles";
 
-export class ColouredContainer extends Component {
-  constructor(props) {
-    super(props);
+export const ColouredContainer = ({ children, ...props }) => {
+  const myRef = useRef();
+  const [clientX, setClientX] = useState(0);
+  const [clientY, setClientY] = useState(0);
+  const [clientWidth, setClientWidth] = useState(0);
+  const [clientHeight, setClientHeight] = useState(0);
 
-    this.myRef = React.createRef();
+  useEffect(() => {
+    function updateDimensions() {
+      const { clientHeight, clientWidth } = myRef.current;
+      setClientWidth(clientWidth);
+      setClientHeight(clientHeight);
+    }
 
-    this.state = {
-      clientX: 0,
-      clientY: 0,
-      clientWidth: 0,
-      clientHeight: 0
+    if (!clientWidth && !clientHeight) {
+      window.addEventListener("resize", updateDimensions);
+      updateDimensions();
+    }
+
+    return function cleanup() {
+      window.removeEventListener("resize", updateDimensions);
     };
+  }, [clientWidth, clientHeight]);
+
+  function onMouseMove({ clientX, clientY }) {
+    setClientX(clientX);
+    setClientY(clientY);
   }
 
-  _onMouseMove({ clientX, clientY }) {
-    this.setState({ clientX, clientY });
-  }
-
-  componentDidMount() {
-    window.addEventListener("resize", () => this.updateDimensions());
-    this.updateDimensions();
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener("resize", () => this.updateDimensions());
-  }
-
-  updateDimensions() {
-    const { clientHeight, clientWidth } = this.myRef.current;
-    this.setState({ clientWidth, clientHeight });
-  }
-
-  render() {
-    const { children } = this.props;
-    const { clientX, clientY, clientWidth, clientHeight } = this.state;
-
-    return (
-      <Container
-        ref={this.myRef}
-        clientX={clientX}
-        clientY={clientY}
-        clientWidth={clientWidth}
-        clientHeight={clientHeight}
-        onMouseMove={this._onMouseMove.bind(this)}
-      >
-        {children}
-      </Container>
-    );
-  }
-}
+  return (
+    <Container
+      ref={myRef}
+      clientX={clientX}
+      clientY={clientY}
+      clientWidth={clientWidth}
+      clientHeight={clientHeight}
+      onMouseMove={onMouseMove}
+      {...props}
+    >
+      {children}
+    </Container>
+  );
+};
--- src/components/ColouredContainer/styles.js
+++ src/components/ColouredContainer/styles.js
@@ -1,35 +1,17 @@
 import styled from "styled-components";
 import { rgba } from "polished";
-import { isBrowser } from "react-device-detect";
-import { COLORS } from "../../constants";
 
-export const Container = styled.main.attrs(
-  ({ clientX, clientY, clientWidth, clientHeight, style }) =>
-    isBrowser
-      ? {
-          style: {
-            ...style,
-            backgroundColor: rgba(
-              Math.round((clientX / clientHeight) * 255),
-              255 - Math.round((clientY / clientWidth) * 255),
-              255 - Math.round((clientX / clientHeight) * 255),
-              0.75
-            )
-          }
-        }
-      : {
-          style: {
-            ...style,
-            backgroundColor: rgba(COLORS.white, 0.005)
-          }
-        }
+export const Container = styled.div.attrs(
+  ({ clientX, clientY, clientWidth, clientHeight, style }) => ({
+    style: {
+      backgroundColor: rgba(
+        Math.round((clientX / clientHeight) * 255),
+        255 - Math.round((clientY / clientWidth) * 255),
+        255 - Math.round((clientX / clientHeight) * 255),
+        0.75
+      )
+    }
+  })
 )`
-  display: flex;
-  flex-direction: column;
-  position: relative;
-  height: 100%;
-  margin: 0;
-  padding: 0;
-
-  transition: background-color 0.2s ease-out;
+  transition: background-color 0.2s linear;
 `;
