72ab232d5f60b25e5461caa962c990a4f4d18b03
Andrew James <contact@ajames.dev>
2020-01-09 20:30:35 +0000

    🐛 fixed FOUC with global styles

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,20 +1,18 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { Transition } from "react-spring/renderprops";
 import { GlobalStyles, Main } from "./styles";
 
 export const Layout = ({ children }) => (
-  <Transition
-    items={true}
-    delay={200}
-    from={{ opacity: 0 }}
-    enter={{ opacity: 1 }}
-    leave={{ opacity: 0 }}
-  >
-    {() => props => (
-      <Main style={props}>
-        <GlobalStyles />
-        {children}
-      </Main>
-    )}
-  </Transition>
+  <Fragment>
+    <GlobalStyles />
+    <Transition
+      items={true}
+      delay={250}
+      from={{ opacity: 0 }}
+      enter={{ opacity: 1 }}
+      leave={{ opacity: 0 }}
+    >
+      {() => props => <Main style={props}>{children}</Main>}
+    </Transition>
+  </Fragment>
 );
