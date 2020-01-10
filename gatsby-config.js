ae3d3ecfc1d0ed9c4d4d5c7cb73c0df8e5bfe36b
Andrew James <contact@ajames.dev>
2020-01-10 10:49:43 +0000

    🔧 Added gatsby source filesystem and transformer remark plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,13 @@
 module.exports = {
-  plugins: [`gatsby-plugin-styled-components`]
+  plugins: [
+    `gatsby-plugin-styled-components`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `blogs`,
+        path: `${__dirname}/blogs`
+      }
+    },
+    `gatsby-transformer-remark`
+  ]
 };
