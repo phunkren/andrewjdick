9a2875ec51bc8bf420897aa51d0890cdf280eab7
Andrew James <contact@ajames.dev>
2020-01-14 16:17:12 +0000

    🎨 Refactored props on current icons

--- src/components/icons/EmailIcon.js
+++ src/components/icons/EmailIcon.js
@@ -2,7 +2,8 @@ import React from "react";
 import { Icon } from "./Icon";
 
 export const EmailIcon = props => (
-  <Icon viewBox="0 0 1000 1000" {...props}>
-    <path d="M499.1 10C229.5 10 10 229.5 10 499.1 10 770.5 229.5 990 499.1 990 768.8 990 990 770.5 990 499.1 990 229.5 770.5 10 499.1 10zm281.8 672.3H219.1V352.2l280 207.4 280-207.4v330.1h1.8zM499.1 526.8l-280-207.4h560l-280 207.4z" />
+  <Icon {...props}>
+    <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path>
+    <polyline points="22,6 12,13 2,6"></polyline>
   </Icon>
 );
