827934a261a10b9a0811142827b77df1d82fab10
Andrew James <contact@ajames.dev>
2020-01-17 00:06:46 +0000

    🔧 Wrapped root element in breadcrumb provider

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,7 @@
+import React from "react";
+import { BreadcrumbProvider } from "./src/components/breadcrumb";
 require("prismjs/themes/prism-okaidia.css");
+
+export const wrapRootElement = ({ element }) => (
+  <BreadcrumbProvider>{element}</BreadcrumbProvider>
+);
