e466e6786c55e4311d7f61384e6845c767964727
Andrew James <contact@ajames.dev>
2020-02-23 23:15:09 +0000

    ✨ Created global and mobile navigation

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,31 +1,76 @@
 import React from 'react';
 import styled from 'styled-components';
-import { rgba } from 'polished';
+import { rgba, position } from 'polished';
+import { Social } from '../components/Social';
+import { MEDIA } from '../styles/media';
 import { COLORS } from '../styles/colors';
 import { Link } from './Link';
 
 const ListItem = styled.li`
   display: inline;
   text-transform: uppercase;
-  padding: 1em 1.5em;
 `;
 
 const List = styled.ul`
+  display: flex;
+  flex-flow: row;
+
   ${ListItem} + ${ListItem} {
     position: relative;
+    margin-left: 2em;
 
     &::before {
       content: '|';
       position: absolute;
-      left: 0;
-      margin-right: 1em;
+      left: -1em;
       color: ${rgba(COLORS.black, 0.5)};
+      transform: translateX(-50%);
     }
   }
 `;
 
+const MobileList = styled.ul`
+  display: flex;
+  flex-flow: column;
+
+  ${ListItem} + ${ListItem} {
+    position: relative;
+    margin-top: 2em;
+  }
+`;
+
+const Nav = styled.nav`
+  display: none;
+
+  ${MEDIA.desktop`
+    display: flex;
+  `}
+`;
+
+const MobileNav = styled.nav`
+  ${position('absolute', '80px', '0', '0', '0')};
+  display: flex;
+  flex-flow: column;
+  padding: 2em 2em 1em;
+  background-image: radial-gradient(
+    70% 70% at 50% 100%,
+    #e8e8e8 0%,
+    #fafafa 100%
+  );
+
+  & > ${List} {
+    flex: 1;
+    display: flex;
+    flex-flow: column;
+  }
+
+  ${MEDIA.desktop`
+    display: none;
+  `}
+`;
+
 const RawNavigation = props => (
-  <nav aria-label="Main" {...props}>
+  <Nav aria-label="Main" {...props}>
     <List>
       <ListItem>
         <Link to="/blog">Blog</Link>
@@ -34,7 +79,22 @@ const RawNavigation = props => (
         <Link to="/cv">Résumé</Link>
       </ListItem>
     </List>
-  </nav>
+  </Nav>
+);
+
+const RawMobileNavigation = props => (
+  <MobileNav aria-label="Main" {...props}>
+    <MobileList>
+      <ListItem>
+        <Link to="/blog">Blog</Link>
+      </ListItem>
+      <ListItem>
+        <Link to="/cv">Résumé</Link>
+      </ListItem>
+    </MobileList>
+    <Social css="margin: auto auto 0" />
+  </MobileNav>
 );
 
 export const Navigation = styled(RawNavigation)``;
+export const MobileNavigation = styled(RawMobileNavigation)``;
