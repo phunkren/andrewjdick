120e602d423f58c34eeec6d7ade4a0eeaf4afb70
Andrew James <contact@ajames.dev>
2020-03-01 16:26:33 +0000

    wip

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -1,81 +1,93 @@
 import React from 'react';
-import { Helmet } from 'react-helmet';
-import { graphql, StaticQuery } from 'gatsby';
+import Helmet from 'react-helmet';
+import { graphql, useStaticQuery } from 'gatsby';
 
-export const SEO = ({ title, description, image, pathname, article }) => (
-  <StaticQuery
-    query={query}
-    render={({
-      site: {
-        siteMetadata: {
-          defaultTitle,
-          titleTemplate,
-          defaultDescription,
-          siteUrl,
-          defaultImage,
-          twitterUsername,
-        },
-      },
-      socialJson: {
-        social: {
-          twitter: { twitterHandle, twitterUrl },
-        },
-      },
-    }) => {
-      const seo = {
-        title: title || defaultTitle,
-        description: description || defaultDescription,
-        image: `${siteUrl}${image || defaultImage}`,
-        url: `${siteUrl}${pathname || '/'}`,
-        type: article ? 'article' : 'website',
-      };
-
-      return (
-        <Helmet>
-          {/* <meta name="description" content={seo.description} />
-            <meta name="image" content={seo.image} /> */}
-          {/* <meta property="og:url" content={seo.url} />
-            <meta property="og:type" content={seo.type} />
-            <meta property="og:title" content={seo.title} />
-            <meta property="og:description" content={seo.description} />
-            <meta property="og:image" content={seo.image} /> */}
-          <meta name="twitter:card" content="summary" />
-          {/* <meta name="twitter:creator" content={twitterHandle} /> */}
-          <meta name="twitter:title" content={seo.title} />
-          <meta name="twitter:description" content={seo.description} />
-          <meta name="twitter:image" content={seo.image} />
-        </Helmet>
-      );
-    }}
-  />
-);
-
-const query = graphql`
-  query SEO {
-    site {
-      siteMetadata {
-        defaultTitle: title
-        defaultDescription: description
-        defaultImage: image
-        titleTemplate
-        siteUrl
-      }
-    }
-    socialJson {
-      social {
-        twitter {
-          twitterHandle: handle
-          twitterUrl: url
+export const SEO = ({ title, description, image, pathname, meta, article }) => {
+  const {
+    site: { siteMetadata },
+    socialJson: { social },
+  } = useStaticQuery(
+    graphql`
+      query {
+        site {
+          siteMetadata {
+            title
+            titleTemplate
+            description
+            siteUrl
+            image
+          }
+        }
+        socialJson {
+          social {
+            twitter {
+              handle
+              url
+            }
+          }
         }
       }
-    }
-  }
-`;
+    `,
+  );
+
+  const seo = {
+    title: title || siteMetadata.title,
+    description: description || siteMetadata.description,
+    image: `${siteMetadata.siteUrl}${image || siteMetadata.image}`,
+    url: `${siteMetadata.siteUrl}${pathname || '/'}`,
+    type: article ? 'article' : 'website',
+  };
+
+  const seoMeta = [
+    {
+      name: `description`,
+      content: seo.description,
+    },
+    {
+      property: `og:title`,
+      content: seo.title,
+    },
+    {
+      property: `og:description`,
+      content: seo.description,
+    },
+    {
+      property: `og:type`,
+      content: seo.type,
+    },
+    {
+      name: `twitter:card`,
+      content: `summary`,
+    },
+    {
+      name: `twitter:creator`,
+      content: social.twitter.handle,
+    },
+    {
+      name: `twitter:title`,
+      content: seo.title,
+    },
+    {
+      name: `twitter:description`,
+      content: seo.description,
+    },
+  ];
+
+  return (
+    <Helmet
+      htmlAttributes={{ lang: 'en' }}
+      title={seo.title}
+      titleTemplate={title && siteMetadata.titleTemplate}
+      meta={seoMeta}
+    />
+  );
+};
 
 SEO.defaultProps = {
   title: null,
   description: null,
   image: null,
+  meta: [],
   pathname: null,
   article: false,
 };
