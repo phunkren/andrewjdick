91bef950a382e317a6c27e7d65e38e3c5888db33
Andrew James <contact@ajames.dev>
2020-03-01 10:04:38 +0000

    wip

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -31,26 +31,20 @@ export const SEO = ({ title, description, image, pathname, article }) => (
       };
 
       return (
-        <>
-          <Helmet
-            htmlAttributes={{ lang: 'en' }}
-            title={seo.title}
-            titleTemplate={title && titleTemplate}
-          >
-            <meta name="description" content={seo.description} />
-            <meta name="image" content={seo.image} />
-            <meta property="og:url" content={seo.url} />
+        <Helmet>
+          {/* <meta name="description" content={seo.description} />
+            <meta name="image" content={seo.image} /> */}
+          {/* <meta property="og:url" content={seo.url} />
             <meta property="og:type" content={seo.type} />
             <meta property="og:title" content={seo.title} />
             <meta property="og:description" content={seo.description} />
-            <meta property="og:image" content={seo.image} />
-            <meta name="twitter:card" content="summary" />
-            <meta name="twitter:creator" content={twitterHandle} />
-            <meta name="twitter:title" content={seo.title} />
-            <meta name="twitter:description" content={seo.description} />
-            <meta name="twitter:image" content={seo.image} />
-          </Helmet>
-        </>
+            <meta property="og:image" content={seo.image} /> */}
+          <meta name="twitter:card" content="summary" />
+          {/* <meta name="twitter:creator" content={twitterHandle} /> */}
+          <meta name="twitter:title" content={seo.title} />
+          <meta name="twitter:description" content={seo.description} />
+          <meta name="twitter:image" content={seo.image} />
+        </Helmet>
       );
     }}
   />
