6856e8e1236e47e16bf6dc9c7d671bbe87aa9d2d
Andrew James <andrew.james@bcgdv.com>
2020-08-05 16:07:27 +0100

    🎨 Added tailwind colors to palette

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,85 +1,15 @@
 import React, { Fragment, useState } from 'react';
 import { Transition } from 'react-spring/renderprops';
 import { animated } from 'react-spring';
-import styled, { createGlobalStyle, css } from 'styled-components';
+import styled, { css } from 'styled-components';
 import { Link } from 'gatsby';
-import { reset } from 'modern-css-reset';
 import { Logo } from '../components/Logo';
 import { Navigation, MobileNavigation } from '../components/Navigation';
 import { IconButton } from '../components/Button';
 import { CrossIcon } from './icons/CrossIcon';
 import { HamburgerIcon } from './icons/HamburgerIcon';
-import Roboto from '../assets/fonts/Roboto-Regular.woff2';
-import Rubik from '../assets/fonts/Rubik-Regular.woff2';
+import { GlobalStyles } from '../styles/global';
 import { MEDIA } from '../styles/media';
-
-const GlobalStyles = createGlobalStyle`
-  ${reset};
-
-  :root { 
-    --color-black: #1d1d1c;
-    --color-white: #fcfcfc;
-    --color-cadetBlue: #5f9ea0;
-    --color-hippyBlue: #4D91A9;
-    --color-wedgewood: #5681ab;
-  }
-
-  @font-face {
-    src: url(${Rubik}) format('woff2');
-    font-family: 'Rubik';
-    font-display: swap;
-  }
-
-  @font-face {
-    src: url(${Roboto}) format('woff2');
-    font-family: 'Roboto';
-    font-display: swap;
-  }
-
-  html {
-    display: flex;
-  }
-
-  body {
-    font-family: 'Roboto', serif;
-    font-size: 16px;
-    font-size: 1rem;
-    color: var(--color-black);
-    background-color: var(--color-white);
-    width: 100%;
-  
-
-    *::selection {
-      var(--color-wedgewood);
-    }
-
-    *::moz-selection {
-      background: var(--color-wedgewood);
-    }
-
-    ${MEDIA.desktopWide`
-      font-size: 1.25rem;
-    `}
-
-    ${MEDIA.print`
-      font-size: 12pt;
-    `}
-  }
-  
-  body, 
-  div#___gatsby, 
-  div#gatsby-focus-wrapper {
-    flex: 1;
-    display: flex;
-    flex-direction: column;
-  }
-
-  h1, h2, h3, h4 {
-    color: var(--color-black);
-    font-family: 'Rubik', sans-serif;
-    font-weight: 300;
-  }
-`;
 
 const Header = styled.header(({ isNavOpen }) => [
   css`
