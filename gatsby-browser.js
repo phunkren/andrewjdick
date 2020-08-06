2af532fe010f645019773a802da5dd156d957a0c
Andrew James <andrew.james@bcgdv.com>
2020-08-06 22:23:21 +0100

    🔧removed breeadcrumb

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,8 +1,1 @@
-import React from 'react';
-import { BreadcrumbProvider } from './src/components/Breadcrumb';
-
 require('prismjs/themes/prism-okaidia.css');
-
-export const wrapRootElement = ({ element }) => (
-  <BreadcrumbProvider>{element}</BreadcrumbProvider>
-);
