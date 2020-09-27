a92dbba48480bd6ec91b02688f19e93e2b1c4528
Andrew James <andrew.james@bcgdv.com>
2020-09-27 21:31:48 +0100

    Added theme toggle to header

--- src/components/Theme.js
+++ src/components/Theme.js
@@ -1,7 +1,25 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
+import { CustomCheckbox } from '@reach/checkbox';
+import { useTheme } from '../utils/useTheme';
 import { THEMES } from '../styles/themes';
 
 export const Theme = ({ theme = 'light', ...props }) => (
   <ThemeProvider theme={THEMES[theme]} {...props} />
 );
+
+export const ThemeToggle = props => {
+  const { theme, update } = useTheme();
+
+  function handleChange(e) {
+    update(e.target.checked ? 'light' : 'dark');
+  }
+
+  return (
+    <CustomCheckbox
+      checked={theme === 'light'}
+      onChange={handleChange}
+      {...props}
+    />
+  );
+};
