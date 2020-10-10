bf268e33360759a5a7c67bfdf0a3d9d79e454181
Andrew James <andrew.james@bcgdv.com>
2020-10-10 17:20:29 +0100

    Removed useTheme from Layout

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Theme } from './Theme';
-import { useTheme } from '../utils/useTheme';
 import { GlobalStyles } from '../styles/global';
 
 export const Layout = styled(({ children, className }) => {
-  const { theme } = useTheme();
-
   return (
-    <Theme theme={theme}>
+    <Theme>
       <>
         <GlobalStyles />
         {children}
