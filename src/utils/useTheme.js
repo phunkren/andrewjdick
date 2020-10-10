85d1b53269b863504003fb4ed6ec3dd0a8a6b0c2
Andrew James <andrew.james@bcgdv.com>
2020-10-10 15:00:21 +0100

    Tweaked useTheme hook

--- src/utils/useTheme.js
+++ src/utils/useTheme.js
@@ -2,17 +2,16 @@ import { useEffect, useState } from 'react';
 import { THEMES, DEFAULT_THEME } from '../styles/themes';
 
 export function useTheme() {
-  const [theme, setTheme] = useState(null);
-  const windowGlobal = typeof window !== 'undefined' && window;
-
   const retrieve = () => windowGlobal?.localStorage?.getItem('theme');
   const persist = () => windowGlobal?.localStorage?.setItem('theme', theme);
   const validate = t => Object.keys(THEMES).includes(t);
   const update = t => validate(t) && setTheme(t);
 
+  const [theme, setTheme] = useState(retrieve());
+  const windowGlobal = typeof window !== 'undefined' && window;
+
   if (theme === null) {
-    const localTheme = retrieve();
-    setTheme(validate(localTheme) ? localTheme : DEFAULT_THEME);
+    setTheme(DEFAULT_THEME);
   }
 
   useEffect(persist, [theme]);
