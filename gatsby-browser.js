eb5c1d33e94e8cfc46137a6d1cefdf6278d9259a
Andrew James <contact@ajames.dev>
2020-10-26 11:20:53 +0000

    Added root and page wraps to gatsby browser

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,4 +1,9 @@
+import React from 'react';
 import 'modern-css-reset';
+import 'prismjs/themes/prism-tomorrow.css';
 import './src/styles/global.css';
+import { Theme } from './src/components/Theme';
+import { Layout } from './src/components/Layout';
 
-require('prismjs/themes/prism-okaidia.css');
+export const wrapRootElement = ({ element }) => <Theme>{element}</Theme>;
+export const wrapPageElement = ({ element }) => <Layout>{element}</Layout>;
