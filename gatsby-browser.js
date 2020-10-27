92557b00db92f17ba34282371260523820202435
Andrew James <contact@ajames.dev>
2020-10-27 00:54:40 +0000

    removed css imports from gatby browser and ssr

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,4 @@
 import React from 'react';
-import 'modern-css-reset';
-import 'prismjs/themes/prism-tomorrow.css';
-import './src/styles/global.css';
 import { Theme } from './src/components/Theme';
 import { Layout } from './src/components/Layout';
 
