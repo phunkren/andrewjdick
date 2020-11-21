dc419cc097880826afa25b7afc325484fd7a2c9e
Andrew James <contact@ajames.dev>
2020-11-21 00:22:10 +0000

    animation speed tweaks

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -59,7 +59,7 @@ const Container = styled(animated.aside)(({ theme, variant }) => [
       }
 
       &::after {
-        transition: opacity 0.2s ease-out 0.1s;
+        transition: opacity 0.2s ease-out;
         opacity: 0;
       }
     `,
